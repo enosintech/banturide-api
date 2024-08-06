0a2bb2c70f1b13cb5e02a9555f50b22a969f0957
enosintech <enos.nsambaj@gmail.com>
2024-08-06 15:53:25 +0200

    test deploy 7 - update url names

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -21,19 +21,19 @@ const router = express.Router();
 router.use(verifyUser);
 
 // Define routes USERS
-router.get('/profile', getUserProfile); // Changed to use the current user
-router.post('/profile/name', editUserName);
-router.post('/profile/upload', uploadProfilePicture);
-router.delete('/profile/remove', removeProfilePicture);
+router.get('/get-user-profile', getUserProfile); // Changed to use the current user
+router.post('/edit-username', editUserName);
+router.post('/uploadUserProfilePicture', uploadProfilePicture);
+router.delete('/removeUserProfilePicture', removeProfilePicture);
 router.post('/toggle-notifications', toggleNotifications);
 router.post('/toggle-driver-should-call', toggleDriverShouldCall);
 
 // Define routes DRIVERS
-router.put('/edit', editDriverProfile); // Changed to use the current user
-router.post('/toggle-availability', toggleDriverAvailability); // Changed to use the current user
-router.get('/info', getDriverInfo); // Changed to use the current user
-router.get('/total-earnings', getTotalEarnings); // Get the total earnings of the driver
-router.post('/update-status', updateDriverStatus); // Update the driver's status
-router.get('/statistics', getDriverStatistics); // Get driver statistics
+router.put('/edit-driver', editDriverProfile); // Changed to use the current user
+router.post('/toggle-driver-availability', toggleDriverAvailability); // Changed to use the current user
+router.get('/get-driver-info', getDriverInfo); // Changed to use the current user
+router.get('/get-driver-total-earnings', getTotalEarnings); // Get the total earnings of the driver
+router.post('/update-driver-status', updateDriverStatus); // Update the driver's status
+router.get('/get-driver-statistics', getDriverStatistics); // Get driver statistics
 
 export default router;
