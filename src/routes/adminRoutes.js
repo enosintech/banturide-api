2e4993f55d87e37a1d2735e42a972b9c8fa7a42c
Mukela Katungu <65640620+Mukela12@users.noreply.github.com>
2024-10-09 22:45:04 +0800

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,11 +1,22 @@
 import express from "express";
-
-import { approveDriverApplication, denyDriverApplication, getAllComplaints, getAllDriverApplications } from "../controllers/adminController.js";
+import { 
+    approveDriverApplication, 
+    denyDriverApplication, 
+    getAllComplaints, 
+    getAllDriverApplications,
+    createAdmin,
+    loginAdmin 
+} from "../controllers/adminController.js";
 
 const router = express.Router();
 
+// Admin creation and login routes
+router.post("/create-admin", createAdmin);
+router.post("/login-admin", loginAdmin);
+
+// Driver applications and complaints routes
 router.get("/get-driver-applications", getAllDriverApplications);
-router.get("/get-complaints", getAllComplaints)
+router.get("/get-complaints", getAllComplaints);
 router.post("/approve-driver-application", approveDriverApplication);
 router.post("/deny-driver-application", denyDriverApplication);
 
