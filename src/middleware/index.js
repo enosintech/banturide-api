1cb21501165375c1f67e4cad372a09e520fb63b7
enosintech <enos.nsambaj@gmail.com>
2024-10-10 22:21:12 +0200

    idk anymore

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -14,10 +14,10 @@ export const verifyUser = async (req, res, next) => {
         const decodedToken = await admin.auth().verifyIdToken(idToken);
         const uid = decodedToken.uid;
 
-        const userRecord = await admin.auth().getUser(uid);
-        if (!userRecord) {
-            return res.status(403).json({ success: false, message: "Unauthorized", info: "User not found"});
-        }
+        // const userRecord = await admin.auth().getUser(uid);
+        // if (!userRecord) {
+        //     return res.status(403).json({ success: false, message: "Unauthorized", info: "User not found"});
+        // }
 
         req.user = decodedToken;
         return next();
